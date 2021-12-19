0fab643e3891e3692ee211029b521fb304b291cb
toddt <toddteis@gmail.com>
2021-12-19 13:58:14 +1000

    Refactored setupGrid() to create div's with CSS Grid styles.

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,13 @@ const displayScreen = document.querySelector('.display-screen');
 setupGrid(16);
 
 function setupGrid(squares) {
- for (let index = 0; index < squares; index++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    displayScreen.appendChild(div);
- }
+   const gridSize = squares * squares;
+   const divSize = 500 / squares;
+   for (let index = 0; index < gridSize; index++) {
+      const div = document.createElement('div');
+      div.classList.add('grid');
+      displayScreen.style.gridTemplateColumns = `repeat(${squares}, ${divSize})`;
+      displayScreen.style.gridTemplateRows = `repeat(${squares}, ${divSize})`;
+      displayScreen.appendChild(div);
+   }
 }
