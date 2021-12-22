5430265abe69f5105fd3f986cff19a1c7706b9e0
toddt <toddteis@gmail.com>
2021-12-22 17:37:55 +1000

    Added event listeners to setUpGrid() and change bk grd to black.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const displayScreen = document.querySelector('.display-screen');
 
-setupGrid(16);
+setupGrid(100);
 
 function setupGrid(squares) {
    const gridSize = squares * squares;
@@ -8,8 +8,15 @@ function setupGrid(squares) {
    for (let index = 0; index < gridSize; index++) {
       const div = document.createElement('div');
       div.classList.add('grid');
-      displayScreen.style.gridTemplateColumns = `repeat(${squares}, ${divSize})`;
-      displayScreen.style.gridTemplateRows = `repeat(${squares}, ${divSize})`;
+      displayScreen.style.gridTemplateColumns = `repeat(${squares}, ${divSize}px)`;
+      displayScreen.style.gridTemplateRows = `repeat(${squares}, ${divSize}px)`;
       displayScreen.appendChild(div);
+
+      let className = `grid${index}`;
+      div.classList.add(`${className}`)
+      let divNumber = document.querySelector(`.${className}`);
+      divNumber.addEventListener('mouseover', () => {
+         div.classList.add('black');
+      })
    }
 }
